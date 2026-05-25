f5d092fb8cb0ace33b9cc6cfd8f8e05f4eeb7a1b
Angelserrano-std <serranoangel34576@gmail.com>
2026-05-25 12:54:15 -0600

    Update MainH6.java

--- probabilidad/HandsOn6/MainH6.java
+++ probabilidad/HandsOn6/MainH6.java
@@ -1,17 +1,14 @@
 public class MainH6 {
     public static void main(String[] args) {
-        System.out.println("A  - nublado");
-        System.out.println("A' - no nublado");
-        System.out.println("B  - llueve");
-        System.out.println("B' - no llueve");
+
 
         DataSetH6 dataset = new DataSetH6();
 
 
+
         dataset.imprimirTabla();
-        System.out.println("");
-        System.out.println("");
         dataset.calcularProbabilidades();
     }
 }
+
 
